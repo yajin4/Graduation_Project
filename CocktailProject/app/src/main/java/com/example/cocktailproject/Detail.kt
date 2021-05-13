7b891acdafa90a485847c8b10621767f4a0fc198
seungmin001 <minnie9808@gmail.com>
2021-05-13 18:36:04 +0900

    detail activity 추가

---
+++ CocktailProject/app/src/main/java/com/example/cocktailproject/Detail.kt
@@ -0,0 +1,11 @@
+package com.example.cocktailproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
