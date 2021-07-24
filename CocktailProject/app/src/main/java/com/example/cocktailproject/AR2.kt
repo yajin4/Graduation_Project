b72285aba2d1d6b4e80b5604432760768f024dcb
seungmin001 <minnie9808@gmail.com>
2021-07-24 22:18:30 +0900

    서버에서 추론하기 위한 통신 기능
    * 기존 tflite를 사용한 추론을 진행시키는 코드를 주석처리
    * 일단 실험단계이므로 10초마다 서버로 캡처를 전송하여 추론 실행
    * 513x513 size로 조절한 bitmap을 file로 변환하여 전송

--- CocktailProject/app/src/main/java/com/example/cocktailproject/AR2.kt
+++ CocktailProject/app/src/main/java/com/example/cocktailproject/AR2.kt
@@ -2,14 +2,23 @@ package com.example.cocktailproject
 
 import android.content.Intent
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.cocktailproject.databinding.ActivityAr2Binding
 import com.otaliastudios.cameraview.CameraListener
 import com.otaliastudios.cameraview.CameraView
 import com.otaliastudios.cameraview.PictureResult
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
 
 //permission은 라이브러리에 내장되어있음.
 class AR2 : AppCompatActivity() {
@@ -27,6 +36,9 @@ class AR2 : AppCompatActivity() {
         //intent 정보 받기
         selectedCocktail= intent.getSerializableExtra("selectedCocktail") as Cocktail
         selectedCocktailDetail= intent.getSerializableExtra("selectedCocktailDetail") as ArrayList<CocktailDetail>
+
+        //서버 연결
+        // connectServer()
 
         //카메라 기능 초기설정
         cameraInit()
@@ -62,7 +74,7 @@ class AR2 : AppCompatActivity() {
 
     private fun cameraInit() {
         //camera component
-        camera=findViewById(R.id.camera)
+        camera=binding.camera
         //LifeCycleOwner 이 액티비티로 연결해줘야함.
         camera.setLifecycleOwner(this)
         //사진 찍은거 event listener
@@ -73,10 +85,23 @@ class AR2 : AppCompatActivity() {
                     //513으로 변환해서 input주기
                     //binding.sample.setImageBitmap(it)
                     if (it != null) {
-                        imageProcess(it)
+                        //imageProcess(it)
+                        var tempfile=File(filesDir.toString()+"current.jpg")
+                        val os= FileOutputStream(tempfile)
+                        it.compress(Bitmap.CompressFormat.JPEG,100,os)
+                        os.flush()
+                        os.close()
+                        connectServer(tempfile)
                     }
-
                 }
+
+//                var file=File(filesDir.toString()+"current.jpg")
+//                result.toFile(file){
+//                    if (it!=null){
+//                        connectServer(it)
+//                        Log.i("connect tag","connected!!!!"+file.isFile.toString())
+//                    }
+//                }
             }
         })
         //2초마다 한번씩 불리게 쓰레드 반복실행
@@ -86,7 +111,7 @@ class AR2 : AppCompatActivity() {
                 //더 작은 용량의 snapshot으로 운영 TODO:잘 안될 경우 바꾸기
                 camera.takePictureSnapshot()
                 //camera.takePicture()
-                mainHandler.postDelayed(this,1500)
+                mainHandler.postDelayed(this,10000)
             }
         })
 
@@ -101,5 +126,49 @@ class AR2 : AppCompatActivity() {
         //set ovelay image
         binding.sample.setImageBitmap(maskedBitmap)
         binding.overlayimage.setImageBitmap(maskedBitmap)
+    }
+
+    // connect server
+    private fun connectServer(currentShot:File) {
+        val ipv4Address="118.223.16.156"
+        val portNum="5000"
+        val postUrl = "http://"+ipv4Address+":"+portNum+"/"
+
+        // bitmap arg로 받기
+        val postBodyText="hello"
+        //val requestBody=postBodyText.toRequestBody()
+        //experiment
+        val requestBody=currentShot.asRequestBody("image/*".toMediaTypeOrNull())
+        val postBodyImage=MultipartBody.Builder()
+            .setType(MultipartBody.FORM)
+            .addFormDataPart("image","inf.jpg",requestBody)
+            .build()
+        postRequest(postUrl, postBodyImage)
+
+    }
+
+    private fun postRequest(postUrl: String, requestBody: RequestBody) {
+        val client= OkHttpClient()
+
+        val request= Request.Builder().url(postUrl).post(requestBody).build()
+
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                //cancel the post on failure
+                call.cancel()
+                Log.i("connect tag","failed!!!!"+e.toString())
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+                // ui thread == main thread
+//                runOnUiThread {
+//                    val responseText = findViewById<TextView>(R.id.responseText)
+//                    responseText.text = "Failed to Connect to Server"
+//                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                // TODO("Not yet implemented")
+                Log.i("connect tag","success!"+ response.body?.string())
+            }
+        })
     }
 }
